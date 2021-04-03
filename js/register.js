197090966d32b7477160d087ecc26aac3c942747
Lucas Caniggia <lucascaniggia5@gmail.com>
2021-04-03 15:59:27 -0300

    JS files added, better styling, still not working 100%

---
+++ js/register.js
@@ -0,0 +1,80 @@
+var validationDiv = document.querySelector('.validationContent');
+var validText = "Validation results:";
+
+var formInDom = function () {
+    if (document.querySelector('.form-item')) {
+        validationDiv.append(validText + " " + 'Form is found' + " ");
+    }
+    else {
+        validationDiv.style.color = 'red';
+        validationDiv.append(validText + " " + 'Form is not found' + " ");
+    }
+}
+
+formInDom();
+
+var fieldsQuant = function () {
+    var fields = document.querySelectorAll('input');
+    var fieldsNumber = fields.length;
+
+    if (fieldsNumber = 4) {
+        validationDiv.append(validText + " " + 'Input fields are correct' + " ");
+    }
+    else {
+        validationDiv.style.color = 'red';
+        validationDiv.append(validText + " " + 'There are missing inputs' + " ");
+    }
+}
+
+fieldsQuant()
+
+var requiredFields = function () {
+    var inputs = document.querySelectorAll('.inputForms').required;
+    if (inputs = true) {
+        validationDiv.append(validText + " " + 'All fields are required' + " ");
+    }
+    else {
+        validationDiv.style.color = 'red';
+        validationDiv.append(validText + " " + 'Required attributes missing' + " ");
+    }
+}
+
+requiredFields();
+
+var relatedLabels = function () {
+    var inputs = document.querySelectorAll('.inputForms').name;
+    var labels = document.querySelectorAll('labels').for;
+    if (inputs === labels) {
+        validationDiv.append(validText + " " + 'All inputs have associated labels' + " ");
+    }
+    else {
+        validationDiv.style.color = 'red';
+        validationDiv.append(validText + " " + 'There are some input label\'s missing' + " ");
+    }
+}
+
+relatedLabels();
+
+var validButt = function () {
+    var submitBtn = document.querySelector('.submitBtn').nodeValue;
+    var submitValue = 'Login';
+    if (submitBtn = submitValue) {
+        validationDiv.append(validText + " " + 'Buttons content is correct' + " ");
+    }
+    else {
+        validationDiv.style.color = 'red';
+        validationDiv.append(validText + " " + 'Buttons content is wrong' + " ");
+    }
+}
+
+validButt();
+
+
+var validation = function () {
+    if ((formInDom = true) && (fieldsQuant = true) && (requiredFields = true) &&
+        (associatedLabels = true) && (validBtns = true)) {
+        validationDiv.textContent = validText + " " + 'Every validation has passed!';
+    }
+}
+
+validation();
